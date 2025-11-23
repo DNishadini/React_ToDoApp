4280c3d82821bd547123ff9019a7e425115cdb70
DNishadini <dinushikaherath150@gmail.com>
2025-11-23 20:29:00 +0530

    Update

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,11 +1,16 @@
 import styles from "./todoitem.module.css";
 export default function TodoItem({ item }) {
+  function handleDelete() {
+    console.log("Delete button clicked");
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         <span>
-          <button className={styles.deleteButton}>X</button>
+          <button onClick={handleDelete} className={styles.deleteButton}>
+            x
+          </button>
         </span>
       </div>
 
